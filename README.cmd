d1001791ca6bf03ba676eda88ff02fffb52f3b5c
Andreas Schmittner <aschmitt@coas.oregonstate.edu>
2019-06-19 16:17:16 -0700

    added website info

--- README.cmd
+++ README.cmd
@@ -1,2 +1,3 @@
 # UVic2.9
 This is the base code of the UVic model version 2.9. The source directory should not be changed. It is the original code without updates. It resides in /usr/local/models/UVic_ESCM/2.9/. Changes should be made to "updates/latest", which contains the latest updates. It should reside in /usr/local/models/UVic_ESCM/2.9/updates/.  
+Additional information about MOBI is available at http://people.oregonstate.edu/~schmita2/Models/MOBI/index.html
