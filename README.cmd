6b8fc193ffffbe7922f4593d7b5e6b37a1f2e6e3
andreasschmittner <aschmitt@coas.oregonstate.edu>
2021-03-05 17:40:42 -0800

    updated README

--- README.cmd
+++ README.cmd
@@ -1,3 +1,6 @@
 # UVic2.9
-This is the base code of the UVic model version 2.9. The source directory should not be changed. It is the original code without updates. It resides in /usr/local/models/UVic_ESCM/2.9/. Changes should be made to "updates/latest", which contains the latest updates. It should reside in /usr/local/models/UVic_ESCM/2.9/updates/.  
-Additional information about MOBI is available at http://people.oregonstate.edu/~schmita2/Models/MOBI/index.html
+This is the base code of the University of Victoria (UVic) climate model version 2.9 used at OSU. The source directory should not be changed. It is the original code without updates. It resides in /usr/local/models/UVic_ESCM/2.9/. Changes should be made to "updates/latest", which contains the latest updates. It should reside in /usr/local/models/UVic_ESCM/2.9/updates/. 
+
+## Further info
+[UVic2.9 webpage](https://osu-ceoas-schmittner.github.io/UVic2.9/)
+
